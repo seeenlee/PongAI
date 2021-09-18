dda07b857c9b080104dfadef59fe280e50d9f22f
Sean Lee <naeseel18@gmail.com>
2021-09-18 18:38:45 -0400

    basic ai

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import pygame
 
 pygame.init()
@@ -5,18 +7,23 @@ pygame.init()
 screen = pygame.display.set_mode((1000, 600))
 pygame.display.set_caption("First Game")
 
+# values associated with all paddles
+PADDLE_WIDTH = 25
+PADDLE_LENGTH = 125
+
+# radius of ball
+RADIUS = 10
+
 
 def game_loop():
+
     # coordinates of ball
     ball_x = 500
     ball_y = 300
 
     # speed of ball
-    x_vel = -1
-    y_vel = .5
-
-    # radius of ball
-    RADIUS = 10
+    x_vel = -7
+    y_vel = 7
 
     # coordinates of right paddle
     right_paddle_x = 925
@@ -25,10 +32,6 @@ def game_loop():
     # coordinates of left paddle
     left_paddle_x = 50
     left_paddle_y = 250
-
-    # values associated with all paddles
-    PADDLE_WIDTH = 25
-    PADDLE_LENGTH = 125
 
     loop = True
     clock = pygame.time.Clock()
@@ -45,45 +48,54 @@ def game_loop():
 
         # border boundaries
         if ball_x < RADIUS or ball_x > 1000 - RADIUS:
-            x_vel = -x_vel
+            ball_x = 500
+            ball_y = 300
+            x_vel = -7
+            y_vel = 7
+            # TODO: move the ball to the middle before it freezes
+            time.sleep(1)
+
         if ball_y < RADIUS or ball_y > 600 - RADIUS:
             y_vel = -y_vel
 
         # bouncing off paddle
         if ball.colliderect(left_paddle):
-            if (left_paddle_y + (PADDLE_LENGTH/2)) < ball_y:
+            if (left_paddle_y + (PADDLE_LENGTH / 2)) <= ball_y:
                 x_vel = -x_vel
                 y_vel = y_vel
-            elif (right_paddle_y + (PADDLE_LENGTH/2)) > ball_y:
+            elif (left_paddle_y + (PADDLE_LENGTH / 2)) > ball_y:
                 x_vel = -x_vel
                 y_vel = y_vel
         if ball.colliderect(right_paddle):
             x_vel = -x_vel
-            if (right_paddle_y + (PADDLE_LENGTH/2)) < ball_y:
+            if (right_paddle_y + (PADDLE_LENGTH / 2)) <= ball_y:
                 if y_vel < 0:
                     y_vel *= 1
-                    print('same slope')
                 else:
                     y_vel = -y_vel
-                    print('changed slope')
-            elif (right_paddle_y + (PADDLE_LENGTH/2)) > ball_y:
+            elif (right_paddle_y + (PADDLE_LENGTH / 2)) > ball_y:
                 if y_vel > 0:
                     y_vel *= 1
-                    print('same slope')
                 else:
                     y_vel = -y_vel
-                    print('change slope')
 
             else:
                 print('why are you here')
 
-
         # paddle control
         pressed = pygame.key.get_pressed()
         if pressed[pygame.K_UP]:
-            right_paddle_y -= 1
-        if pressed[pygame.K_DOWN] :
-            right_paddle_y += 1
+            right_paddle_y -= 8
+        if pressed[pygame.K_DOWN]:
+            right_paddle_y += 8
+
+        # AI
+
+        if (left_paddle_y + (PADDLE_LENGTH / 2)) < ball_y:
+            left_paddle_y += 10
+        if (left_paddle_y + (PADDLE_LENGTH / 2)) > ball_y:
+            left_paddle_y -= 10
+
 
         # game quit
         for event in pygame.event.get():
@@ -93,6 +105,7 @@ def game_loop():
         # update of screen
         pygame.display.update()
         screen.fill((0, 0, 0))
+        clock.tick(60)
 
 
 game_loop()
