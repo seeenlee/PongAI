5192cbae9d73cada33fbd63d6d0de43c63109fc8
Sean Lee <naeseel18@gmail.com>
2021-09-18 16:35:27 -0400

    half paddle bounce

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
 import pygame
+
 pygame.init()
 
-# Resolution of Game window
-X_DIMENSION = 1000
-Y_DIMENSION = 600
-screen = pygame.display.set_mode((X_DIMENSION, Y_DIMENSION))
+screen = pygame.display.set_mode((1000, 600))
 pygame.display.set_caption("First Game")
-
-# values associated with all paddles
-PADDLE_LENGTH = 25
-PADDLE_WIDTH = 125
-
-# radius of ball
-RADIUS = 10
 
 
 def game_loop():
-
     # coordinates of ball
     ball_x = 500
     ball_y = 300
 
     # speed of ball
-    x_vel = 1
-    y_vel = 1
+    x_vel = -1
+    y_vel = .5
+
+    # radius of ball
+    RADIUS = 10
 
     # coordinates of right paddle
     right_paddle_x = 925
@@ -33,26 +26,66 @@ def game_loop():
     left_paddle_x = 50
     left_paddle_y = 250
 
+    # values associated with all paddles
+    PADDLE_WIDTH = 25
+    PADDLE_LENGTH = 125
+
     loop = True
     clock = pygame.time.Clock()
 
     while loop:
-
+        ball_x -= x_vel
+        ball_y -= y_vel
         # setup of objects
         left_paddle = pygame.draw.rect(screen, (255, 255, 255),
-                                       pygame.Rect(left_paddle_x, left_paddle_y, PADDLE_LENGTH, PADDLE_WIDTH))
+                                       pygame.Rect(left_paddle_x, left_paddle_y, PADDLE_WIDTH, PADDLE_LENGTH))
         right_paddle = pygame.draw.rect(screen, (255, 255, 255),
-                                        pygame.Rect(right_paddle_x, right_paddle_y, PADDLE_LENGTH, PADDLE_WIDTH))
-        cir = pygame.draw.circle(screen, (255, 255, 255), (ball_x, ball_y), RADIUS)
+                                        pygame.Rect(right_paddle_x, right_paddle_y, PADDLE_WIDTH, PADDLE_LENGTH))
+        ball = pygame.draw.circle(screen, (255, 255, 255), (ball_x, ball_y), RADIUS)
 
         # border boundaries
         if ball_x < RADIUS or ball_x > 1000 - RADIUS:
             x_vel = -x_vel
         if ball_y < RADIUS or ball_y > 600 - RADIUS:
             y_vel = -y_vel
-        ball_x -= x_vel
-        ball_y -= y_vel
 
+        # bouncing off paddle
+        if ball.colliderect(left_paddle):
+            if (left_paddle_y + (PADDLE_LENGTH/2)) < ball_y:
+                x_vel = -x_vel
+                y_vel = y_vel
+            elif (right_paddle_y + (PADDLE_LENGTH/2)) > ball_y:
+                x_vel = -x_vel
+                y_vel = y_vel
+        if ball.colliderect(right_paddle):
+            x_vel = -x_vel
+            if (right_paddle_y + (PADDLE_LENGTH/2)) < ball_y:
+                if y_vel < 0:
+                    y_vel *= 1
+                    print('same slope')
+                else:
+                    y_vel = -y_vel
+                    print('changed slope')
+            elif (right_paddle_y + (PADDLE_LENGTH/2)) > ball_y:
+                if y_vel > 0:
+                    y_vel *= 1
+                    print('same slope')
+                else:
+                    y_vel = -y_vel
+                    print('change slope')
+
+            else:
+                print('why are you here')
+
+
+        # paddle control
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_UP]:
+            right_paddle_y -= 1
+        if pressed[pygame.K_DOWN] :
+            right_paddle_y += 1
+
+        # game quit
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 loop = False
