a7207c7ecf892803744781918775ed282b2caf33
Sean Lee <naeseel18@gmail.com>
2021-09-18 11:33:25 -0400

    added ball

--- main.py
+++ main.py
@@ -17,12 +17,26 @@ rightPaddleX = 925
 rightPaddleY = 250
 
 def game_loop():
+    ballX = 500
+    ballY = 300
+    xVel = 1
+    yVel = 1
+    radius = 10
     loop = True
+
     clock = pygame.time.Clock()
     while loop:
         leftPaddle = pygame.draw.rect(screen, (255,255,255), pygame.Rect(leftPaddleX, leftPaddleY, paddleLength, paddleWidth))
         rightPaddle = pygame.draw.rect(screen, (255,255,255), pygame.Rect(rightPaddleX, rightPaddleY, paddleLength, paddleWidth))
+        cir = pygame.draw.circle(screen, (255, 255, 255), (ballX, ballY), radius)
+        if ballX < radius or ballX > 1000 - radius:
+            xVel = -xVel
+        if ballY < radius or ballY > 600 - radius:
+            yVel = -yVel
+        ballX -= xVel
+        ballY -= yVel
         pygame.display.update()
+        screen.fill((0,0,0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 loop = False
